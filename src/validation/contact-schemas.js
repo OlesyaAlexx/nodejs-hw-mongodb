2e6142c0737cb940200b72fbec95e7dea8521986
OlesyaAlexx <152548843+OlesyaAlexx@users.noreply.github.com>
2024-09-26 00:32:35 +0300

    mod 2

--- src/validation/contact-schemas.js
+++ src/validation/contact-schemas.js
@@ -9,18 +9,18 @@ export const contactAddSchema = Joi.object({
     'any.required': 'Username must be',
   }),
   phoneNumber: Joi.string()
-    .regex(/^[0-9]{12}$/)
-    .messages({ 'string.pattern.base': `Phone number must have 12 digits.` })
+    .pattern(/^\+380\d{9}$/)
+    .messages({ 'string.pattern.base': `Phone number must have 9 digits.` })
     .required(),
-  email: Joi.string().email().required(),
+  email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid(...typeList),
 });
 
 export const contactUpdateSchema = Joi.object({
   name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string().regex(/^[0-9]{12}$/),
-  email: Joi.string().email(),
+  phoneNumber: Joi.string().pattern(/^\+380\d{9}$/),
+  email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid(...typeList),
 });
